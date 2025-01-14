0b391816cea5d8ea112b3565df57aaad93f1750d
Testermonkey <Testermonkey@users.noreply.github.com>
2025-01-14 10:54:02 -0800

    14

---
+++ eTicket/Models/Producer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int id { get; set; }
+        public string ProfilePicture { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
+    }
+}
--- eTicket/Startup.cs
+++ eTicket/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eTicket.Data;
 
 namespace eTicket
 {
@@ -23,6 +24,8 @@ namespace eTicket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // DbContext configuration
+            services.AddDbContext<AppDbContext>();
             services.AddControllersWithViews();
         }
 
