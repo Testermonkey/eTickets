89e52b74a7cc85eb8a728c83917a0e981e6ac591
Testermonkey <Testermonkey@users.noreply.github.com>
2025-01-20 19:55:31 -0800

    2 hours on a hidden issue in Movies/Create.cshtml but fixed. Recreated the file and repasted everything and it worked. Yea? :-|

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
 using System;
@@ -33,11 +34,15 @@ namespace eTickets.Controllers
         }
 
         //GET: Movies/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            ViewData["Weclome"] = "Welcome to our store";
-            ViewBag.Description = "This is the store descritption";
+            var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+
+            ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id","Name");
+            ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
             return View();
         }
+
     }
 }
