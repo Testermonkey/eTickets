752122ec463dd5f2ec520e49b4edd8b02452168c
Testermonkey <Testermonkey@users.noreply.github.com>
2025-01-24 13:10:33 -0800

    91 added register user functionality Need to add logout button

---
+++ eTickets/Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Full name")]
+        [Required(ErrorMessage = "Full name is required")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage ="Confirm password is required")]
+        [Compare("Password", ErrorMessage ="Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
