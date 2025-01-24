560042ebac39b75359a8fdf0b6e6d12e20e15b5f
Testermonkey <Testermonkey@users.noreply.github.com>
2025-01-24 10:45:03 -0800

    89 added functionality for login validation and success

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace eTickets.Controllers
 {
@@ -13,7 +14,7 @@ namespace eTickets.Controllers
         private readonly AppDbContext _context;
 
 
-        public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager, AppDbContext context )
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -23,6 +24,30 @@ namespace eTickets.Controllers
         }
 
         public IActionResult Login() => View(new LoginVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Movies");
+                    }
+                }
+                TempData["Error"] = "Wrong Credentials. Please try again";
+                return View(loginVM);
+            }
+            TempData["Error"] = "Wrong Credentials. Please try again";
+            return View(loginVM);
+        }
 
     }
 }
